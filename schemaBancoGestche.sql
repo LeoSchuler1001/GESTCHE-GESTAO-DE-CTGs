45bd8509c9ab7ac2e64bf2e2adbbf4115b491099
Leonardo Schuler <leonardo_schuler55@hotmail.com>
2026-08-07 22:04:40 -0300

    script banco usuario e modulo sócios

---
+++ schemaBancoGestche.sql
@@ -0,0 +1,103 @@
+CREATE TABLE endereco(
+    pk_idEndereco SERIAL PRIMARY KEY,
+    rua VARCHAR(100),
+    numero INT,
+    cep CHAR(8),
+    bairro VARCHAR(100),
+    cidade VARCHAR(100),
+    estado CHAR(2),
+
+    CONSTRAINT estado_ck CHECK (
+        estado IN (
+            'AC','AL','AP','AM','BA','CE','DF','ES','GO','MA',
+            'MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN',
+            'RS','RO','RR','SC','SP','SE','TO',
+            'US','UK','AU','UY','AR', 'DK'
+        )
+    ),
+    CONSTRAINT cep_ck CHECK (cep ~ '^[0-9]{8}$')
+);
+
+CREATE TABLE usuario(
+    pk_idUsuario SERIAL PRIMARY KEY,
+    cpfUsuario CHAR(11) NOT NULL UNIQUE,
+    nomeUsuario VARCHAR(100) NOT NULL,
+    telefoneUsuario VARCHAR(11),
+    cargoUsuario VARCHAR(100) NOT NULL,
+    senhaHash VARCHAR(60) NOT NULL,
+    respostaSeguranca VARCHAR(100) NOT NULL,
+    fk_idEndereco INT,
+
+    CONSTRAINT cpf_ck CHECK (cpfUsuario ~ '^[0-9]{11}$'),
+    CONSTRAINT fone_ck CHECK (telefoneUsuario IS NULL OR telefoneUsuario ~ '^[0-9]{10,11}$'),
+    CONSTRAINT fk_usuario_endereco FOREIGN KEY(fk_idEndereco) REFERENCES endereco(pk_idEndereco) ON DELETE SET NULL
+);
+
+CREATE TABLE logAuditoria(
+    pk_idLog SERIAL PRIMARY KEY,
+    dataHoraLog TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    descricaoLog TEXT NOT NULL,
+    fk_idUsuario INT,
+    nomeUsuario VARCHAR(100) NOT NULL,
+
+    CONSTRAINT fk_log_usuario FOREIGN KEY(fk_idUsuario) REFERENCES usuario(pk_idUsuario) ON DELETE SET NULL
+);
+
+CREATE TABLE socio(
+    pk_idSocio SERIAL PRIMARY KEY,
+    cpfSocio CHAR(11) NOT NULL UNIQUE,
+    nomeSocio VARCHAR(100) NOT NULL,
+    telefoneSocio VARCHAR(11),
+    dataNascSocio DATE NOT NULL,
+    emailSocio VARCHAR(100),
+    ativoSocio BOOLEAN,
+    fk_idEndereco INT,
+
+    CONSTRAINT fk_socio_endereco FOREIGN KEY(fk_idEndereco) REFERENCES endereco(pk_idEndereco) ON DELETE SET NULL
+);
+
+CREATE TABLE dependente(
+    pk_idDependente SERIAL PRIMARY KEY,
+    nomeDependente VARCHAR(100) NOT NULL,
+    cpfDependente CHAR(11) NOT NULL UNIQUE,
+    dataNascDependente DATE NOT NULL,
+    fk_idSocio INT NOT NULL,
+
+    CONSTRAINT fk_dependente_socio FOREIGN KEY(fk_idSocio) REFERENCES socio(pk_idSocio) ON DELETE CASCADE
+);
+
+CREATE TABLE departamento(
+    pk_idDepartamento SERIAL PRIMARY KEY,
+    nomeDepartamento VARCHAR(100) NOT NULL,
+    descricaoDepartamento TEXT
+);
+
+CREATE TABLE socio_departamento(
+    fk_idSocio INT NOT NULL,
+    fk_idDepartamento INT NOT NULL,
+
+    CONSTRAINT fk_socio FOREIGN KEY(fk_idSocio) REFERENCES socio(pk_idSocio) ON DELETE CASCADE,
+    CONSTRAINT fk_departamento FOREIGN KEY(fk_idDepartamento) REFERENCES departamento(pk_idDepartamento) ON DELETE CASCADE,
+
+    PRIMARY KEY (fk_idSocio, fk_idDepartamento)
+);
+
+CREATE TABLE debito(
+    pk_idDebito SERIAL PRIMARY KEY,
+    tipoDebito VARCHAR(100) NOT NULL,
+    valorDebito NUMERIC(10,2) NOT NULL,
+    vencimentoDebito DATE NOT NULL,
+    dtPgmtDebito DATE,
+    fk_idSocio INT NOT NULL,
+
+    CONSTRAINT valor_debito_ck CHECK (valorDebito > 0),
+    CONSTRAINT fk_debito_socio FOREIGN KEY(fk_idSocio) REFERENCES socio(pk_idSocio) ON DELETE CASCADE
+);
+
+CREATE TABLE conta(
+    pk_idConta SERIAL PRIMARY KEY,
+    nomeConta VARCHAR(100) NOT NULL,
+    corConta VARCHAR(7) NOT NULL,
+    
+)
+
