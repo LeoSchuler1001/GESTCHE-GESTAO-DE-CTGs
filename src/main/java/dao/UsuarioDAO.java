868dbaf0113a03b887bf976e3cefa09e0588b953
Leonardo Schuler <leonardo_schuler55@hotmail.com>
2026-08-15 18:34:31 -0300

    criei a função listar usuarios e atualizar usuarios da classe usuario dao

--- src/main/java/dao/UsuarioDAO.java
+++ src/main/java/dao/UsuarioDAO.java
@@ -2,6 +2,8 @@ package dao;
 
 import java.sql.Statement;
 import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -25,6 +27,7 @@ public class UsuarioDAO {
     }
 
     //MÉTODOS
+    //cadastra um novo usuario no banco de dados
     public void cadastrarUsuario(Usuario usuario) throws SQLException {
         //cria o comando sql
         String sql = "INSERT INTO usuario (cpfUsuario, nomeUsuario, telefoneUsuario, cargoUsuario, senhaHash, respostaSeguranca, ativoUsuario, fk_idEndereco) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
@@ -68,6 +71,7 @@ public class UsuarioDAO {
         }
     }
 
+    //busca um usuario passando o seu id
     public Usuario buscarPorId(int id) throws SQLException {
         //cria o comando sql
         String sql = "SELECT * FROM usuario WHERE pk_idUsuario = ?";
@@ -123,6 +127,55 @@ public class UsuarioDAO {
 
         //salva no banco de dados
         logAuditoriaDAO.cadastrarLog(logAuditoria, usuario);
+    }
+
+    //lista todos os usuários ativos
+    public List<Usuario> listarTodosAtivos() throws SQLException {
+        String sql = "SELECT * FROM usuario WHERE ativoUsuario = TRUE ORDER BY nomeUsuario ASC";
+
+        List<Usuario> listaUsuarios = new ArrayList<>();
+
+        try (PreparedStatement stmt = conexao.getConexao().prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                listaUsuarios.add(montarObjUsuario(rs));
+            }
+        }
+        return listaUsuarios;
+    }
+
+    //atualiza um usuário
+    public void atualizarUsuario(Usuario usuario) throws SQLException {
+        String sql = "UPDATE usuario SET cpfUsuario = ?, nomeUsuario = ?, telefoneUsuario = ?, cargoUsuario = ?, senhaHash = ?, respostaSeguranca = ?, ativoUsuario = ?, fk_idEndereco = ? WHERE pk_idUsuario = ?";
+        
+        try (PreparedStatement stmt = conexao.getConexao().prepareStatement(sql)) {
+            stmt.setString(1, usuario.getCpfUsuario());
+            stmt.setString(2, usuario.getNomeUsuario());
+
+            //verifica se o usuario tem telefone cadastrado
+            if (usuario.getTelefoneUsuario() != null && !usuario.getTelefoneUsuario().isBlank()) {
+                stmt.setString(3, usuario.getTelefoneUsuario());
+            } else {
+                stmt.setNull(3, Types.VARCHAR);
+            }
+
+            stmt.setString(4, usuario.getCargoUsuario());
+            stmt.setString(5, usuario.getSenhaHash());
+            stmt.setString(6, usuario.getRespostaSeguranca());
+            stmt.setBoolean(7, usuario.isAtivoUsuario());
+
+            // verifica se o usuario tem endereço cadastrado
+            if (usuario.getEndereco() != null) {
+                stmt.setInt(8, usuario.getEndereco().getIdEndereco());
+            } else {
+                stmt.setNull(8, Types.INTEGER);
+            }
+
+            stmt.setInt(9, usuario.getIdUsuario());
+
+            //executa o comando sql
+            stmt.executeUpdate();
+        }
     }
 
     //método auxiliar, que vai montar o objeto usuário após a consulta sql
