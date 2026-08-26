c8f188c73e5ce738478832049b0f4f87e142105b
Leonardo Schuler <leonardo_schuler55@hotmail.com>
2026-08-25 22:21:16 -0300

    implementei a funcionalidade do botão sair da tela inicial do secretário

--- src/main/java/controller/TelaInSecretarioController.java
+++ src/main/java/controller/TelaInSecretarioController.java
@@ -1,13 +1,20 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
+import app.App;
 import dao.ConexaoBanco;
 import dao.SocioDAO;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -61,6 +68,18 @@ public class TelaInSecretarioController {
     @FXML
     private TableView<Socio> tabelaSociosInadimplentes;
 
+    //BOTÕES
+    @FXML
+    void sairAction(ActionEvent event) throws IOException {
+        boolean confirmaSaida = emitirAlerta("Deseja realmente sair?", AlertType.CONFIRMATION);
+
+        if (confirmaSaida) {
+            App.trocarTela("TelaLogin");
+        } else {
+            System.out.println("Ação cancelada pelo usuário.");
+        }
+    }
+
     //FUNÇÕES
     public void initialize() throws SQLException {
         ConexaoBanco conexao = new ConexaoBanco();
@@ -89,5 +108,17 @@ public class TelaInSecretarioController {
         //atribui os valores da lista nas tabelas
         tabelaSociosEmdia.setItems(FXCollections.observableArrayList(listaSociosEmDia));
         tabelaSociosInadimplentes.setItems(FXCollections.observableArrayList(listaSociosInadimplentes));
+    }
+
+   private boolean emitirAlerta(String mensagem, AlertType tipoAlerta) {
+        Alert alerta = new Alert(tipoAlerta);
+        alerta.setTitle("Confirmação");
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensagem);
+
+        Optional<ButtonType> resultado = alerta.showAndWait();
+
+        // Verifica se o usuário clicou no botão OK
+        return resultado.isPresent() && resultado.get() == ButtonType.OK;
     }
 }
