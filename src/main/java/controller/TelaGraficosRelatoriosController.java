d00ecff8e880f84a5fd3b532154aa7abec54cafd
Leonardo Schuler <leonardo_schuler55@hotmail.com>
2026-09-19 10:57:18 -0300

    estruturei a tela de gráficos e relatórios

--- src/main/java/controller/TelaGraficosRelatoriosController.java
+++ src/main/java/controller/TelaGraficosRelatoriosController.java
@@ -1,15 +1,46 @@
 package controller;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import app.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class TelaGraficosRelatoriosController {
+
+    @FXML
+    private Button botaoGerarPdf;
+
+    @FXML
+    private Label campoNumeroDependentes;
+
+    @FXML
+    private Label campoNumeroSocios;
+
+    @FXML
+    private Label campoNumeroTotal;
+
+    @FXML
+    private PieChart graficoAtivosInativos;
+
+    @FXML
+    private PieChart graficoEtnias;
+
+    @FXML
+    private PieChart graficoFaixaEtaria;
+
+    @FXML
+    private PieChart graficoHomensMulheres;
 
     @FXML
     private TableColumn<?, ?> lembretes;
@@ -30,7 +61,16 @@ public class TelaGraficosRelatoriosController {
     private Hyperlink linkSociosDependentes;
 
     @FXML
+    private Hyperlink linkSair;
+
+    @FXML
     private TableView<?> tabelaLembretes;
+
+    //BOTÕES
+    @FXML
+    void gerarPdfAction(ActionEvent event) {
+
+    }
 
     @FXML
     void configuracoesAction(ActionEvent event) throws IOException {
@@ -55,5 +95,30 @@ public class TelaGraficosRelatoriosController {
     @FXML
     void sociosDependentesAction(ActionEvent event) throws IOException {
         App.trocarTela("TelaSociosDependentes");
+    }
+    
+    @FXML
+    void sairAction(ActionEvent event) throws IOException {
+        boolean confirmaSaida = emitirAlerta("Deseja realmente sair?", AlertType.CONFIRMATION);
+
+        if (confirmaSaida) {
+            App.trocarTela("TelaLogin");
+        } else {
+            System.out.println("Ação cancelada pelo usuário.");
+        }
+    }
+
+    //MÉTODOS
+    //método auxiliar para emitir alertas
+    private boolean emitirAlerta(String mensagem, AlertType tipoAlerta) {
+        Alert alerta = new Alert(tipoAlerta);
+        alerta.setTitle("Confirmação");
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensagem);
+
+        Optional<ButtonType> resultado = alerta.showAndWait();
+
+        // Verifica se o usuário clicou no botão OK
+        return resultado.isPresent() && resultado.get() == ButtonType.OK;
     }
 }
